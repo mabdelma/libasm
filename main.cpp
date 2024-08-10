5e0f8a001ddd689945c958c1fb22e14c27a6de47
mabdelma <mabdelma@student.42abudhabi.ae>
2024-08-10 20:33:38 +0400

    Fixing strcmp error

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mabdelma <mabdelma@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 14:20:21 by mabdelma          #+#    #+#             */
-/*   Updated: 2024/08/07 19:04:19 by mabdelma         ###   ########.fr       */
+/*   Updated: 2024/08/10 20:32:03 by mabdelma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,65 @@
 
 int		main(void)
 {
-	int		i;
-	long	r;
-	char	buffer[100];
-	char	*tmp;
-	char	*tmp2;
+	// int		i;
+	// long	r;
+	// char	buffer[100];
+	// char	*tmp;
+	// char	*tmp2;
 
-	r = 0;
-	i = 0;
-	while (i < 100)
-		buffer[i++] = 0;
+	// r = 0;
+	// i = 0;
+	// while (i < 100)
+	// 	buffer[i++] = 0;
 
-	printf("--strlen\n");
-	STRLEN("")
-	STRLEN("toto")
-	STRLEN("totototo")
-	STRLEN("0123456789abcdef")
-	STRLEN("42")
-	STRLEN("1")
-	printf("-done\n");
+	// printf("--strlen\n");
+	// STRLEN("")
+	// STRLEN("mj")
+	// STRLEN("mjmj")
+	// STRLEN("0123456789abcdef")
+	// STRLEN("42")
+	// STRLEN("1")
+	// printf("-done\n");
 
 	printf("\n--strcmp\n");
 	STRCMP("", "")
-	STRCMP("toto", "toto")
-	STRCMP("", "toto")
-	STRCMP("toto", "")
-	STRCMP("toto", "totobar")
-	printf("%s:%s = %d\n", "TOTO", "", ft_strcmp("TOTO", NULL));
-	printf("`%s`:%s = %d\n", "", "TOTO", ft_strcmp(NULL, "TOTO"));
-	printf("%s:%s = %d\n", "", "", ft_strcmp(NULL, NULL));
+	STRCMP("mj", "aoto")
+	STRCMP("", "mj")
+	STRCMP("mj", "")
+	STRCMP("mj", "mjbar")
 	printf("-done\n");
 
-	printf("\n--strcpy\n");
-	printf("`%s` (`toto`)\n", ft_strcpy(buffer, "toto"));
-	printf("`%s` (empty)\n", ft_strcpy(buffer, ""));
-	printf("`%s` (`long message`)\n", ft_strcpy(buffer, "long message"));
-	printf("`%s` (NULL > not modified)\n", ft_strcpy(buffer, NULL));
-	printf("-done\n");
+	// printf("\n--strcpy\n");
+	// printf("`%s` (`mj`)\n", ft_strcpy(buffer, "mj"));
+	// printf("`%s` (empty)\n", ft_strcpy(buffer, ""));
+	// printf("`%s` (`long message`)\n", ft_strcpy(buffer, "long message"));
+	// printf("`%s` (NULL > not modified)\n", ft_strcpy(buffer, NULL));
+	// printf("-done\n");
 
-	printf("\n--write\n");
-	WRITE("toto", 4L)
-	WRITE("totototo", 4L)
-	WRITE("totototo", 8L)
-	WRITE("toto", 2L)
-	printf("-done\n");
+	// printf("\n--write\n");
+	// WRITE("mj", 4L)
+	// WRITE("mjmj", 4L)
+	// WRITE("mjmj", 8L)
+	// WRITE("mj", 2L)
+	// printf("-done\n");
 
-	printf("\n--read (Makefile)\n");
-	READ(buffer, 50)
-	READ(buffer, 25)
-	READ(buffer, 4)
-	READ(buffer, 26)
-	READ(buffer, 14)
-	READ(buffer, 0)
-	printf("-done\n");
+	// printf("\n--read (Makefile)\n");
+	// READ(buffer, 50)
+	// READ(buffer, 25)
+	// READ(buffer, 4)
+	// READ(buffer, 26)
+	// READ(buffer, 14)
+	// READ(buffer, 0)
+	// printf("-done\n");
 
-	tmp2 = strdup("Medo");
-	printf("\n-ft_strdup\n");
-	tmp = ft_strdup(tmp2); 
+	// tmp2 = strdup("Medo");
+	// printf("\n-ft_strdup\n");
+	// tmp = ft_strdup(tmp2); 
 	
-	printf("%s:%s\n", tmp2, tmp); 
-	printf("-done\n");
-	free(tmp2);
-	free(tmp);
+	// printf("%s:%s\n", tmp2, tmp); 
+	// printf("-done\n");
+	// free(tmp2);
+	// free(tmp);
 
 	return (0);
 }
